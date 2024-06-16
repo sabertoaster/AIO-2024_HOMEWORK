b8dae836d3f0e3f77a8f2431ff95896145c9570e
Saber <62453787+sabertoaster@users.noreply.github.com>
2024-06-16 07:44:08 +0700

    feat: add Homework Week 03 Module 01 (#7) (#8)
    * feat: add Homework Week 03 Module 01
    
    * fix: update sonarlint
    
    * fix: update sonar smelly code
    
    * fix: remove sonar coverage
    
    * fix: add develop to build.yml
    
    * Revert "fix: remove sonar coverage"
    
    This reverts commit b85a74b6a5d9fc5e2ddd42bb35594820c33d8fb7.
    
    * fix: actually, i prefer it this way
    
    * feat: add tox file
    
    * fix: move tox.ini to new place due to log comment
    
    * fix: revert everything before breakfast
    
    * fix: actually another test wont hurt
    
    * fix: add py39
    
    * fix: remove py39
    
    * fix: can i fix it?
    
    * Update tox.ini
    
    * Create test_Github_AC.py
    
    * Update test_Github_AC.py
    
    * fix: revert all

---
+++ Module 1/Week 3/TuLuan_1_softmax_class.py
@@ -0,0 +1,69 @@
+# Disable GitHub Copilot for this file
+from torch import Tensor
+from torch.nn import Module
+import numpy as np
+
+
+class Softmax(Module):
+    def __init__(self, data=None):
+        super(Softmax, self).__init__()
+        if data is not None:
+            self.data = self.forward(data)
+        else:
+            self.data = None
+
+    def __str__(self):
+        return f"{self.data}"
+
+    def forward(self, x):
+        """
+        Compute the softmax of vector x
+        :param x: torch.Tensor
+        :return: torch.Tensor
+        """
+        exp_x = np.exp(x)
+        return exp_x / exp_x.sum()
+
+
+class softmax_stable(Module):
+    def __init__(self, data=None):
+        super(softmax_stable, self).__init__()
+        if data is not None:
+            self.data = self.forward(data)
+        else:
+            self.data = None
+
+    def __str__(self):
+        return f"{self.data}"
+
+    def forward(self, x):
+        """
+        Compute the softmax of vector x
+        :param x: torch.Tensor
+        :return: torch.Tensor
+        """
+        c = max(x)
+        exp_x = np.exp(x - c)
+        return exp_x / exp_x.sum()
+
+
+if __name__ == "__main__":
+    # Examples 1
+    data = Tensor([1, 2, 3])
+    softmax = Softmax()
+    output = softmax(data)
+    print(output)
+    # tensor([0.0900, 0.2447, 0.6652])
+
+    # Examples 2
+    data = Tensor([1000, 1001, 1002])
+    stable = softmax_stable()
+    output = stable(data)
+    print(output)
+    # tensor([0.0900, 0.2447, 0.6652])
+
+    # Examples 3
+    data = Tensor([4, 5, 7])
+    stable = softmax_stable()
+    output = stable(data)
+    print(output)
